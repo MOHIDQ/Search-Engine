1b66ab10e4d732a75a407286bddddd81615e41f5
mohidqureshi <mohidq99@gmail.com>
2021-04-19 18:23:28 -0400

    Finished frontend

--- search-engine-fe/src/App.js
+++ search-engine-fe/src/App.js
@@ -1,76 +1,60 @@
 import React, {Component} from 'react';
 import Result from "./Result"
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-
 class App extends Component {
   constructor() {
     super();
-    this.state = {searchVal: "", boost: true, numResults: 10, url: ""}
-    this.history = []
-
+    this.state = {searchVal: "", boost: true, numResults: 10, results: [], loading: false}
   }
 
-  testFetch = () => {
+  searchFetch = () => {
     console.log("Clicked")
-    fetch("http://localhost:3000/fruits?q=apple&limit=10&boost=false")
+    this.setState({loading: true, results: []})
+    fetch(`http://localhost:3000/fruits?q=${this.state.searchVal}&limit=${this.state.numResults}&boost=${this.state.boost}`)
         .then((response) => response.json())
         .then((json) => {
-          //checks if property is there, meaning the youtube video was found
-            console.log(json)
+            //console.log(json)
+
+            this.setState({results: json, loading: false})
         })
         .catch((error) => {
           console.error(error);
         });
   }
 
-  searchClick = () => {
-    let url = `/search?q=${this.state.searchVal}&boost=${this.state.boost}&limit=${this.state.numResults}`
-    this.setState({url: url})
-    window.open(url)
-  }
-
   render() {
     return(
       <div>
-        <Router>
-        <Switch>
-          <Route exact path="/search" component={Result}>
-            <Result/>
-          </Route>
-          <Route path="/">
             <div className="homeDiv"> 
               
               <div className="card bg-light text-dark">
                 <div className="card-header">
-                <h1>
-                  <span style={{color: "#4285F4"}}>M</span>
-                  <span style={{color: "#DB4437"}}>o</span>
-                  <span style={{color: "#F4B400"}}>h</span>
-                  <span style={{color: "#4285F4"}}>i</span>
-                  <span style={{color: "#0F9D58"}}>d</span>
-                  <span style={{color: "#DB4437"}}>'</span>
-                  <span style={{color: "#4285F4"}}>s</span>
+                  <h1>
+                    <span style={{color: "#4285F4"}}>M</span>
+                    <span style={{color: "#DB4437"}}>o</span>
+                    <span style={{color: "#F4B400"}}>h</span>
+                    <span style={{color: "#4285F4"}}>i</span>
+                    <span style={{color: "#0F9D58"}}>d</span>
+                    <span style={{color: "#DB4437"}}>'</span>
+                    <span style={{color: "#4285F4"}}>s</span>
 
-                  <span style={{color: "#DB4437"}}> S</span>
-                  <span style={{color: "#F4B400"}}>e</span>
-                  <span style={{color: "#4285F4"}}>a</span>
-                  <span style={{color: "#0F9D58"}}>r</span>
-                  <span style={{color: "#DB4437"}}>c</span>
-                  <span style={{color: "#4285F4"}}>h</span>
+                    <span style={{color: "#DB4437"}}> S</span>
+                    <span style={{color: "#F4B400"}}>e</span>
+                    <span style={{color: "#4285F4"}}>a</span>
+                    <span style={{color: "#0F9D58"}}>r</span>
+                    <span style={{color: "#DB4437"}}>c</span>
+                    <span style={{color: "#4285F4"}}>h</span>
 
-                  <span style={{color: "#DB4437"}}> E</span>
-                  <span style={{color: "#F4B400"}}>n</span>
-                  <span style={{color: "#4285F4"}}>g</span>
-                  <span style={{color: "#0F9D58"}}>i</span>
-                  <span style={{color: "#DB4437"}}>n</span>
-                  <span style={{color: "#4285F4"}}>e</span>
-                  
-                </h1>
+                    <span style={{color: "#DB4437"}}> E</span>
+                    <span style={{color: "#F4B400"}}>n</span>
+                    <span style={{color: "#4285F4"}}>g</span>
+                    <span style={{color: "#0F9D58"}}>i</span>
+                    <span style={{color: "#DB4437"}}>n</span>
+                    <span style={{color: "#4285F4"}}>e</span>
+                    <a href="https://github.com/MOHIDQ/Search-Engine" rel="noreferrer" target="_blank" className="fa fa-github" title="Github Profile"></a>
+
+                  </h1>
+                
                 </div>
                 
               </div>
@@ -78,7 +62,7 @@ class App extends Component {
                 <div className="card-body">
                   <div className="search"> 
                     <input className="form-control searchInp" value={this.state.searchVal} onChange={(val) => {this.setState({searchVal: val.target.value})} }></input>
-                    <button onClick={() => this.searchClick()} type="button" className="btn btn-outline-dark">Search</button>
+                    <button onClick={() => this.searchFetch()} type="button" className="btn btn-outline-dark">Search</button>
                   </div>
                   <div className="settings">
                     <input type="Number" className="form-control numResults" placeholder="# of Results" value={this.state.numResults} onChange={(val) => {this.setState({numResults: val.target.value})}}></input>
@@ -91,13 +75,20 @@ class App extends Component {
                     
                 </div>  
               </div>
+              {this.state.loading ? (
+                <div> 
+                  <br></br>
+                <div className="spinner-border text-primary"></div>
+                <div className="spinner-border text-success"></div>
+                <div className="spinner-border text-warning"></div>
+                <div className="spinner-border text-danger"></div>
+                </div>
+                  ) : (
+                ""
+              )}
+              <Result searchResults={this.state.results}/>
               
             </div>
-          </Route>
-          
-        </Switch>
-        
-        </Router>
         
       </div>
     );
